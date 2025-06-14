ff4fac2a7d00ef6f6d73655c2eaa03f683e67a4c
AhmedUkaye <112416327+AhmedUkaye@users.noreply.github.com>
2025-06-14 22:44:42 +0530

    Update variables.tf

--- assessment/assessment/terraform/variables.tf
+++ assessment/assessment/terraform/variables.tf
@@ -1,5 +1,6 @@
-variable "vpc_cidr" { default = "10.0.0.0/16" }
-variable "public_subnet_cidr" { default = "10.0.1.0/24" }
-variable "private_subnet_cidr" { default = "10.0.2.0/24" }
-variable "instance_type" { default = "t3.xlarge" }
-variable "bucket_name" { default = "terraform-state-storage" }
+variable "vpc_cidr"                { default = "10.0.0.0/16" }
+variable "public_subnet_cidr"      { default = "10.0.1.0/24" }
+variable "private_subnet_cidr"     { default = "10.0.2.0/24" }
+variable "instance_type"           { default = "t3.xlarge" }
+variable "bucket_name"             { default = "terraform-state-storage-unique" }
+variable "ami_id"                  { default = "ami-021a584b49225376d" } # Update for your region
