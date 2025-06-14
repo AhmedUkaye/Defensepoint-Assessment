15807dcb5dffcba190077fe01b8a2641b2b2f014
AhmedUkaye <112416327+AhmedUkaye@users.noreply.github.com>
2025-06-14 21:50:09 +0530

    Update main.tf

--- assessment/assessment/terraform/main.tf
+++ assessment/assessment/terraform/main.tf
@@ -7,13 +7,13 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = var.public_subnet_cidr
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
 }
  
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = var.private_subnet_cidr
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
 }
  
 resource "aws_internet_gateway" "igw" {
